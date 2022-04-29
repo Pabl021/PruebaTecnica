ba065377fd861831392d9c9df61a94f7846aa0bd
Pabl021 <josepablobc21@gmail.com>
2022-04-29 11:33:40 -0600

    prueba terminada

--- PruebaTecnica/Models/TipoPropiedad.cs
+++ PruebaTecnica/Models/TipoPropiedad.cs
@@ -1,8 +1,12 @@
-﻿namespace PruebaTecnica.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaTecnica.Models
 {
     public class TipoPropiedad
     {
         public int Id { get; set; }
+
         public string Descripcion { get; set; }
+        
     }
 }
